dd3f211bea9256643e303967382b850c35391b3d
k.zhdanov <k.zhdanov@newlinetechnologies.net>
2022-01-19 13:07:43 +0200

    test marge

--- dbo/Functions/Creation-Scripts/Func_1.sql
+++ dbo/Functions/Creation-Scripts/Func_1.sql
@@ -1,12 +1,14 @@
 
 CREATE FUNCTION [dbo].[Func_1] (@number int)
 RETURNS char(2)
+
+    some text
 AS
 BEGIN
 DECLARE @result char(2);
 IF @number > 9
-SET @result = convert(char(2), @number);
+SET = convert(char(2), @number);
 ELSE
 SET @result = convert(char(2), '0' + convert(varchar, @number));
-RETURN @result;
+
 END;
